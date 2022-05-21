2f36c3a0e34b2d6da2044afe89180f14a725502d
kweehongquan <105926992+kweehongquan@users.noreply.github.com>
2022-05-22 06:09:30 +0800

    Update staffController_class.php
    Terminate functions if there are no open orders. Error message will not be shown to user.

--- inc/staffController_class.php
+++ inc/staffController_class.php
@@ -3,12 +3,19 @@ class staffController extends staffEntity{
     protected function retrieveOpenOrders(){
         // Call entity class to retrieve ID of open orders.
         $openOrder = $this->getOpenOrdersID();
+
+        // Return 0 if there are no open orders currently. 
+        if ($openOrder == 0){
+            return 0;
+        }
+        
+        // Initialize array to retrieve order items.
         $orderItem = array();
 
         // For each ID, retrieve the items tagged to the ID and push them into their respective array.
         for($i=0; $i<count($openOrder); $i++){
-            $orderItem = $this->getOrderItems($openOrder[$i]['id']);
-            $openOrder[$i]+= $orderItem;
+           $orderItem = $this->getOrderItems($openOrder[$i]['id']);
+           $openOrder[$i]+= $orderItem;
         }
         
         return $openOrder;
